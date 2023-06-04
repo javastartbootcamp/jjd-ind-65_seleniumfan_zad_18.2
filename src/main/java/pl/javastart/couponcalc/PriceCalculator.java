be93bdbe4a0639cbee2699ca6a710b800cb210ef
Tomasz Iwanicki <tiko35@o2.pl>
2023-06-05 00:25:07 +0200

    Solution

--- src/main/java/pl/javastart/couponcalc/PriceCalculator.java
+++ src/main/java/pl/javastart/couponcalc/PriceCalculator.java
@@ -1,13 +1,55 @@
 package pl.javastart.couponcalc;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PriceCalculator {
 
     public double calculatePrice(List<Product> products, List<Coupon> coupons) {
-        // TODO
+        if (Objects.isNull(products) && Objects.isNull(coupons)) {
+            return 0;
+        }
 
-        return 0;
+        if (Objects.isNull(coupons)) {
+            return products.stream()
+                    .mapToDouble(Product::getPrice)
+                    .sum();
+        }
+
+        Map<Category, Integer> categoryWithDiscountMap = coupons.stream()
+                .collect(Collectors.toMap(Coupon::getCategory, Coupon::getDiscountValueInPercents));
+
+        Map<Category, Double> categoryPriceMap = new HashMap<>();
+        for (Product product : products) {
+            categoryPriceMap.put(product.getCategory(), categoryPriceMap.getOrDefault(product.getCategory(), 0.0) + product.getPrice());
+        }
+
+        double maxDiscountAmount = 0;
+        double sum = 0;
+        for (Map.Entry<Category, Double> entry : categoryPriceMap.entrySet()) {
+            sum += entry.getValue();
+            if (categoryWithDiscountMap.containsKey(entry.getKey())) {
+                double discountAmount = entry.getValue() * (categoryWithDiscountMap.get(entry.getKey()) / 100.);
+                if (discountAmount > maxDiscountAmount) {
+                    maxDiscountAmount = discountAmount;
+                }
+            }
+        }
+
+        Optional<Coupon> couponWithoutCategory = coupons.stream()
+                .filter(coupon -> Objects.isNull(coupon.getCategory()))
+                .findFirst();
+
+        double discountAllProducts = 0;
+        if (couponWithoutCategory.isPresent()) {
+            discountAllProducts = sum * (couponWithoutCategory.get().getDiscountValueInPercents() / 100.);
+        }
+        return discountAllProducts > maxDiscountAmount ? roundToTwoPlaces(sum - discountAllProducts) : roundToTwoPlaces(sum - maxDiscountAmount);
     }
 
+    double roundToTwoPlaces(double number) {
+        return BigDecimal.valueOf(number).setScale(2, RoundingMode.HALF_UP).doubleValue();
+    }
 }
--- src/test/java/pl/javastart/couponcalc/PriceCalculatorTest.java
+++ src/test/java/pl/javastart/couponcalc/PriceCalculatorTest.java
@@ -6,14 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static pl.javastart.couponcalc.Category.*;
 
 public class PriceCalculatorTest {
+    private final PriceCalculator priceCalculator = new PriceCalculator();
 
     @Test
     public void shouldReturnZeroForNoProducts() {
-        // given
-        PriceCalculator priceCalculator = new PriceCalculator();
-
         // when
         double result = priceCalculator.calculatePrice(null, null);
 
@@ -23,11 +22,9 @@ public class PriceCalculatorTest {
 
     @Test
     public void shouldReturnPriceForSingleProductAndNoCoupons() {
-
         // given
-        PriceCalculator priceCalculator = new PriceCalculator();
         List<Product> products = new ArrayList<>();
-        products.add(new Product("Masło", 5.99, Category.FOOD));
+        products.add(new Product("Masło", 5.99, FOOD));
 
         // when
         double result = priceCalculator.calculatePrice(products, null);
@@ -37,15 +34,49 @@ public class PriceCalculatorTest {
     }
 
     @Test
-    public void shouldReturnPriceForSingleProductAndOneCoupon() {
-
+    public void shouldReturnPriceForMultipleProductsAndNoCoupons() {
         // given
-        PriceCalculator priceCalculator = new PriceCalculator();
         List<Product> products = new ArrayList<>();
-        products.add(new Product("Masło", 5.99, Category.FOOD));
+        products.add(new Product("Masło", 5.39, FOOD));
+        products.add(new Product("Mleko", 2.39, FOOD));
+        products.add(new Product("Chleb", 8, FOOD));
+        products.add(new Product("Lamborghini", 14_000_000, CAR));
+        products.add(new Product("Play gituar", 40, ENTERTAINMENT));
+
+        // when
+        double result = priceCalculator.calculatePrice(products, null);
+
+        // then
+        assertThat(result).isEqualTo(14_000_055.78);
+    }
+
+    @Test
+    public void shouldReturnPriceForMultipleProductsAndCouponsWithoutPassingCategory() {
+        // given
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Masło", 5.39, FOOD));
+        products.add(new Product("Mleko", 2.39, FOOD));
+        products.add(new Product("Chleb", 8, FOOD));
 
         List<Coupon> coupons = new ArrayList<>();
-        coupons.add(new Coupon(Category.FOOD, 20));
+        coupons.add(new Coupon(ENTERTAINMENT, 20));
+        coupons.add(new Coupon(CAR, 20));
+
+        // when
+        double result = priceCalculator.calculatePrice(products, null);
+
+        // then
+        assertThat(result).isEqualTo(15.78);
+    }
+
+    @Test
+    public void shouldReturnPriceForSingleProductAndOneCoupon() {
+        // given
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Masło", 5.99, FOOD));
+
+        List<Coupon> coupons = new ArrayList<>();
+        coupons.add(new Coupon(FOOD, 20));
 
         // when
         double result = priceCalculator.calculatePrice(products, coupons);
@@ -54,5 +85,118 @@ public class PriceCalculatorTest {
         assertThat(result).isEqualTo(4.79);
     }
 
+    @Test
+    public void shouldReturnPriceForMultipleProductsAndOneCoupon() {
+        // given
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Masło", 5.99, FOOD));
+        products.add(new Product("Ser", 15.99, FOOD));
+        products.add(new Product("Chleb", 8, FOOD));
+        products.add(new Product("Dżem", 6.22, FOOD));
 
+        List<Coupon> coupons = new ArrayList<>();
+        coupons.add(new Coupon(FOOD, 10));
+
+        // when
+        double result = priceCalculator.calculatePrice(products, coupons);
+
+        // then
+        assertThat(result).isEqualTo(32.58);
+    }
+
+    @Test
+    public void shouldReturnPriceForSingleProductAndCouponWithoutCategory() {
+        // given
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Masło", 5.99, FOOD));
+
+        List<Coupon> coupons = new ArrayList<>();
+        coupons.add(new Coupon(null, 10));
+
+        // when
+        double result = priceCalculator.calculatePrice(products, coupons);
+
+        // then
+        assertThat(result).isEqualTo(5.39);
+    }
+
+    @Test
+    public void shouldReturnPriceForThreeProductsAndCouponWithoutCategory() {
+        // given
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Masło", 6, FOOD));
+        products.add(new Product("Cinema ticket", 60, ENTERTAINMENT));
+        products.add(new Product("radio", 34, CAR));
+
+        List<Coupon> coupons = new ArrayList<>();
+        coupons.add(new Coupon(null, 10));
+
+        // when
+        double result = priceCalculator.calculatePrice(products, coupons);
+
+        // then
+        assertThat(result).isEqualTo(90);
+    }
+
+    @Test
+    public void shouldReturnPriceForFourProductsAndCoupons() {
+        // given
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Masło", 6, FOOD));
+        products.add(new Product("Cinema ticket", 60, ENTERTAINMENT));
+        products.add(new Product("radio", 34, CAR));
+        products.add(new Product("Ser", 15, FOOD));
+
+        List<Coupon> coupons = new ArrayList<>();
+        coupons.add(new Coupon(FOOD, 10));
+        coupons.add(new Coupon(ENTERTAINMENT, 20));
+
+        // when
+        double result = priceCalculator.calculatePrice(products, coupons);
+
+        // then
+        assertThat(result).isEqualTo(103);
+    }
+
+    @Test
+    public void shouldReturnPriceWithOverallDiscount() {
+        // given
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Masło", 6, FOOD));
+        products.add(new Product("Cinema ticket", 60, ENTERTAINMENT));
+        products.add(new Product("radio", 34, CAR));
+        products.add(new Product("Ser", 15, FOOD));
+
+        List<Coupon> coupons = new ArrayList<>();
+        coupons.add(new Coupon(FOOD, 10));
+        coupons.add(new Coupon(ENTERTAINMENT, 20));
+        coupons.add(new Coupon(null, 50));
+
+        // when
+        double result = priceCalculator.calculatePrice(products, coupons);
+
+        // then
+        assertThat(result).isEqualTo(57.5);
+    }
+
+    @Test
+    public void shouldReturnPriceWithCategoryDiscountInsteadOverallDiscount() {
+        // given
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Masło", 6, FOOD));
+        products.add(new Product("Cinema ticket x 4", 200, ENTERTAINMENT));
+        products.add(new Product("radio", 34, CAR));
+        products.add(new Product("Ser", 15, FOOD));
+
+        List<Coupon> coupons = new ArrayList<>();
+        coupons.add(new Coupon(FOOD, 10));
+        coupons.add(new Coupon(ENTERTAINMENT, 50));
+        coupons.add(new Coupon(null, 10));
+
+        // when
+        double result = priceCalculator.calculatePrice(products, coupons);
+
+        // then
+        assertThat(result).isEqualTo(155);
+    }
 }
